040a75cc28cbd34c5724ac2ec1868d100e4995b1
Dean <mccarthey123@outlook.com>
2018-10-13 15:21:51 +0800

    实现socket通信

--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+
 const port = 8770
+const server = app.listen(port, () => {
+    console.log(`Server on port http://localhost:${port}/!`)
+})
+const io = require('socket.io').listen(server)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '..', 'dist/vue-chat/index.html'))
 });
 
 io.on('connection', socket => {
 	console.log('a user connected!')
+	socket.on('disconnect', () => {
+		console.log('user disconnected')
+	})
+	socket.on('chat message', msg => {
+		io.emit('chat message', msg)
+	})
 })
 
 app.use('/vue-chat', express.static('../dist/vue-chat/'))
-
-app.listen(port, () => {
-	console.log(`Server on port http://localhost:${port}/!`)
-})
--- vue-chat/src/views/Home.vue
+++ vue-chat/src/views/Home.vue
@@ -1,13 +1,57 @@
 <template>
   <div class="home">
     <h1>this is home page</h1>
+    <button @click="closeSocket">close socket</button>
+    <div>
+      <p v-for="(msg, index) in msgList" :key="index">{{ msg }}</p>
+    </div>
+    <input type="text" v-model='inputText'>
+    <button @click="sendMessage">Send</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import io from 'socket.io-client'
+
 export default {
   name: 'home',
+  data() {
+    return {
+      socketObj: '',
+      msgList: [],
+      inputText: ''
+    }
+  },
+  methods: {
+    initSocket() {
+      this.socketObj = io('http://10.0.21.16:8770/')
+      this.socketObj.on('connect', () => {
+        console.log('start')
+      })
+      console.log(this.socketObj)
+      this.socketObj.on('disconnect', () => {
+        console.log('chat closed')
+      })
+      this.socketObj.on('chat message', msg => {
+        this.msgList.push(msg)
+      })
+    },
+    sendMessage() {
+      if (!this.inputText) {
+        console.log('Message cannot be empty!')
+        return 
+      }
+      this.socketObj.emit('chat message', this.inputText)
+      this.inputText = ''
+    },
+    closeSocket() {
+      this.socketObj.disconnect()
+    }
+  },
+  created(){
+    this.initSocket()
+  }
 }
 </script>
