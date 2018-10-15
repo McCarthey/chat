4bc39d9697a6a2089a40f520aa8baf776f51ea7f
Dean <mccarthey123@outlook.com>
2018-10-15 18:33:54 +0800

    add muse-ui

--- vue-chat/src/main.js
+++ vue-chat/src/main.js
@@ -5,11 +5,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { theme } from 'muse-ui'
+import Toast from 'muse-ui-toast';
+
 Vue.use(MuseUI)
+Vue.use(Toast)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
