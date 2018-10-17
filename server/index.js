180554a62442ddec0b7cbfd5723076478a7aa525
Dean <mccarthey123@outlook.com>
2018-10-17 18:10:57 +0800

    body-parser/multer

--- server/index.js
+++ server/index.js
@@ -2,15 +2,26 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const bodyParser = require('body-parser')
+const multer = require('multer')
+
 const port = 8770
 const server = app.listen(port, () => {
     console.log(`Server on port http://localhost:${port}/!`)
 })
 const io = require('socket.io').listen(server)
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/upload', (req, res) => {
+    console.log(req.body)
+    res.send('for uploading imgs');
+})
 
 io.on('connection', socket => {
     console.log('a user connected!')
