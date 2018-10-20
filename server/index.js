f7f4183ecdcd6f1730020178265584291142fd0b
Dean <mccarthey123@outlook.com>
2018-10-20 16:35:46 +0800

    注册接口出入用户数据

--- server/index.js
+++ server/index.js
@@ -15,6 +15,14 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage }) // for parsing multipart/form-data
 
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+let db
+
+MongoClient.connect(url, (err, client) => {
+    db = client.db('chat')
+})
+
 const port = 8770
 const server = app.listen(port, () => {
     console.log(`Server on port http://localhost:${port}/!`)
@@ -32,8 +40,15 @@ app.post('/upload', upload.single('file'), (req, res, next) => {
     res.send(`http://10.0.21.16:8770/uploads/${req.file.filename}`);
 })
 
-app.post('/signUp', (req, res) => {
+app.post('/signUp', async (req, res) => {
     console.log(req.body)
+    let data = req.body
+    const dbResult = await db.collection('users').insertOne({
+        username: data.username,
+        password: data.password
+    })
+    console.log(dbResult)
+    
     res.send({
         code: 0,
         msg: 'Success!'
